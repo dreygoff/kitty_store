0e240a2b54467f0cd404ee18517069a3079a9594
dreyg <dreygoff@gmail.com>
2026-01-12 20:13:55 +0300

    login and signup activities added

--- app/src/main/java/com/example/kittystore/SignupActivity.kt
+++ app/src/main/java/com/example/kittystore/SignupActivity.kt
@@ -3,33 +3,29 @@ package com.example.kittystore
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnLogin: Button = findViewById(R.id.btn_login)
+        val goToLogin: TextView = findViewById(R.id.to_login_from_signup_second)
         val btnSignup: Button = findViewById(R.id.btn_signup)
 
-        btnLogin.setOnClickListener {
+        goToLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnSignup.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
     }
